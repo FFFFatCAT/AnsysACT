a68e8c23088d93dc360a9f5b92b7a7a5d5dadf14
Louis-PeideWang <louis1990.wang@outlook.com>
2021-04-22 20:36:27 +0800

    First ACT extension code.HiFive!

--- HiFive/HiFiveMain.py
+++ HiFive/HiFiveMain.py
@@ -1,5 +1,17 @@
 # -*- coding:utf-8 -*-
-# Project:
+# Project: HiFive
 # User:CNLOWAN2
 # Author: Louis-PeideWang
 # CreateDate: 2021/4/22 19:48
+
+clr.AddReference("Ans.UI.Toolkit")
+clr.AddReference("Ans.UI.Toolkit.Base")
+from Ansys.UI.Toolkit import *
+
+
+def init(context):
+    ExtAPI.Log.WriteMessage("Init ACT HiFive...")
+
+
+def ShowMessageBox(analysis_obj):
+    MessageBox.Show("Hi five! My first ACT extension is a success!")
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# AnsysACT
-ACT Extension for ANSYS Workbench
+# ACT Extension for ANSYS Workbench
+- - -
+参考ANSYS官方帮助文件，step by step，复现官方案例，同时结合业务需求，定制化开发扩展功能。
+- - -
+## HiFive
+My first ACT extension for Workbench Mechanical. 
+No useful function, only realization of the connection between XML file, Python script and Mechanical.
+### Output
+通过点击ToolBar button，链接Python script，显示MessageBox。
+![alt](./images/HiFive.png)
